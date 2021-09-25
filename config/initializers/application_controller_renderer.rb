fdf10fa7d8929703495568e066d885818f4b0c5c
akiraeguchi <oneok.enjoymylife@gmail.com>
2021-09-25 16:07:09 +0000

    アソシエーション途中

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7fd49699212b24ae99fee995cf6ac53739d621004a5710962b21a0fb755c9fe8b40a8b1476c686ff9f82fac20df7e49aeb56ecb9b4ebdd4f3b0bc8230b4ef67e'
